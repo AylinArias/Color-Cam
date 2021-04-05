6df79eabebcd5f1a3786eb08a8d070384ed89d34
aylin <ayliin.af@gmial.com>
2021-04-05 18:44:51 -0500

    ColorCam

---
+++ Color Cam/app/src/main/java/com/example/colorcam/LuminosityAnalyzer.kt
@@ -0,0 +1,34 @@
+package com.raywenderlich.colorcam
+
+import android.util.Log
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import java.nio.ByteBuffer
+import java.util.concurrent.TimeUnit
+
+class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+  private var lastTimeStamp = 0L
+  private val TAG = this.javaClass.simpleName
+
+  override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+    val currentTimestamp = System.currentTimeMillis()
+    val intervalInSeconds = TimeUnit.SECONDS.toMillis(1)
+    val deltaTime = currentTimestamp - lastTimeStamp
+    if (deltaTime >= intervalInSeconds){
+      val buffer = image.planes[0].buffer
+      val data = buffer.toByteArray()
+      val pixels = data.map { it.toInt() and 0xFF }
+      val luma = pixels.average()
+      lastTimeStamp = currentTimestamp
+      Log.d(TAG, "Average luminosity: $luma")
+    }
+  }
+
+  private fun ByteBuffer.toByteArray(): ByteArray {
+    rewind()
+    val data = ByteArray(remaining())
+    get(data)
+    return data
+  }
+
+}
